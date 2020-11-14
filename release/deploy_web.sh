bc327150c5c79d378e0b3bdb25dd51e686d80dc7
Dustin Carlino <dabreegster@gmail.com>
2020-11-14 13:12:23 -0800

    Add a progress bar for the initial .wasm loading. #377
    It's now easier to understand the possibly slow downloading of the 80MB
    .wasm.

--- release/deploy_web.sh
+++ release/deploy_web.sh
@@ -11,14 +11,4 @@ rm -f system
 aws s3 sync . s3://abstreet/$VERSION
 # Undo that symlink hiding
 git checkout system
-# Set the content type for .wasm files, to speed up how browsers load them
-aws s3 cp \
-       s3://abstreet/$VERSION \
-       s3://abstreet/$VERSION \
-       --exclude '*' \
-       --include '*.wasm' \
-       --no-guess-mime-type \
-       --content-type="application/wasm" \
-       --metadata-directive="REPLACE" \
-       --recursive
 echo "Have the appropriate amount of fun: http://abstreet.s3-website.us-east-2.amazonaws.com/$VERSION"
